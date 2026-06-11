e00530f090042e045cf784850db8d4452f8d1605
Juan Albertino <juanalbertinokiller@gmail.com>
2026-06-11 09:44:01 +0300

    finition controller

--- src/java/com/framework/controller/FrontController.java
+++ src/java/com/framework/controller/FrontController.java
@@ -1,4 +1,33 @@
 package framework.controller;
 
-public class FrontController {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontController extends HttpServlet {
+
+    public void affichage(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getPathInfo();
+
+        if (path == null) {
+            path = request.getServletPath();
+        }
+
+        response.setContentType("text/html");
+        response.getWriter().println("Bienvenue sur la page d'accueil\nUrl actuel: " + path);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        affichage(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        affichage(request, response);
+    }
 }
